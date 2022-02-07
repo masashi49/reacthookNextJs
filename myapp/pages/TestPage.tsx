66ca1dd125941a67cfeda30451f2f70b6f45789a
yamadamasashi <shisama94@gmail.com>
2022-02-07 09:38:50 +0900

    pageの型

--- myapp/pages/TestPage.tsx
+++ myapp/pages/TestPage.tsx
@@ -1,8 +1,14 @@
-import React from "react"
+import Reactm, { useState } from "react"
 import type { NextPage } from 'next'
 
 interface Props {
   text: string
+}
+
+interface UserData {
+  id: number;
+  name: string;
+  login: boolean
 }
 
 const TestComponent: React.FC<Props> = (props) => {
@@ -14,9 +20,24 @@ const TestComponent: React.FC<Props> = (props) => {
 }
 
 const TestPage: NextPage = () => {
+  const [count, setCount] = useState<number | null>(0);
+  const [user, setUser] = useState<UserData>({
+    id: 1,
+    name: "太郎",
+    login: false
+  })
+  const nueId = () => {
+    setUser({ ...user, login: true })
+  }
   return (
     <div>
       <h1><TestComponent text="hello" /></h1>
+      {count} <br />
+      {user.id}
+      {user.name}
+      {user.login && "ログインです"}
+      <br />
+      <button onClick={nueId}></button>
     </div>
   )
 }
