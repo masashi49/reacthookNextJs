8f4b453257a1ede825b8f6d13145ea0043e56d30
yamadamasashi <shisama94@gmail.com>
2022-02-25 00:40:09 +0900

    イベントの方定義

--- myapp/components/TestCompo.tsx
+++ myapp/components/TestCompo.tsx
@@ -1,20 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 
 interface Props {
     text: string
 }
 
+interface userData {
+    id: number;
+    name: string;
+}
+
 const TestCompo: React.VFC<Props> = (props) => {
+    const [count, setCount] = useState<number | null>(0)
+    const [user, setuser] = useState<userData>({ id: 100, name: "" })
+    const [aaarr, setAttr] = useState<number[]>([1, 2, 3, 454, 54, 5, 34,])
+    const [inputeData, setInputData] = useState("")
+    
+    const handleInputchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputData(e.target.value)
+    }
+
 
     return (
         <>
             <p>
                 {props.text}
+                {user.id}
+                {user.name}
+                {
+                    aaarr.map((number) =>
+                        <span>{number}</span>
+                    )
+                }
             </p>
+            <p>{count}</p>
             <p>propsの方定義</p>
             <p>interfaceで受け取るものを定義するだけ。</p>
             <p>React.VFCとReact.FC</p>
             <p>ここを読む:  https://qiita.com/tttocklll/items/c78aa33856ded870e843</p>
+            <input type="text" value={inputeData} onChange={handleInputchange}/>
+            <p>{inputeData}です！</p>
         </>
     )
 }
